f2e56668b7eea3479d0463e562b74bcbcb3ed418
Dave Moreno <wabbie@byui.edu>
2019-05-13 16:23:25 -0600

    Add second hello

--- src/Burdett/main.java
+++ src/Burdett/main.java
@@ -5,5 +5,6 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("Hello my name is Jordan!");
+        System.out.println("Hello Jordan, my name is Dave.");
     }
 }
